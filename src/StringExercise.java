5760f13c73bcb346da701f84bb0b98d251dd91d8
Rod-S <rodsanati@gmail.com>
2020-12-04 11:08:02 -0600

    complete StringExercise

---
+++ src/StringExercise.java
@@ -0,0 +1,13 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String quote1 = "We don't need no education\n" + "We don't need no thought control";
+        String quote2 = "Check \"this\" out!, \"s inside of \"s!";
+        String quote3 = "In windows, the main drive is usually C:\\";
+        String quote4 = "I can do backslashes \\, double backslashes \\\\,\n" + "and the amazing triple backslash \\\\\\!";
+
+        System.out.println(quote1);
+        System.out.println(quote2);
+        System.out.println(quote3);
+        System.out.println(quote4);
+    }
+}
