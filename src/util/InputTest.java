511a13c703037b759c50a8cd7f9d4df771747091
Rod-S <rodsanati@gmail.com>
2020-12-09 11:11:04 -0600

    complete OOP exercise 2 util

---
+++ src/util/InputTest.java
@@ -0,0 +1,43 @@
+package util;
+
+public class InputTest {
+
+    public static void main(String[] args){
+//        Input input1 = new Input();
+//        System.out.println(input1.getString());
+
+        //        Input input1 = new Input();
+//        System.out.println(input1.getString("Enter a string."));
+
+        //        Input input2 = new Input();
+//        System.out.println(input2.yesNo());
+
+//        Input input2 = new Input();
+//        System.out.println(input2.yesNo("Enter an int."));
+
+//        Input input3 = new Input();
+//        System.out.println(input3.getDouble("Enter a double."));
+
+        //        Input input2 = new Input();
+//        System.out.println(input2.yesNo());
+
+
+        //        Input input2 = new Input();
+//        System.out.println(input2.yesNo("Type in yes or y for true otherwise will return false"));
+
+//        Input input3= new Input();
+//        input3.getInt(1,100);
+
+//        Input input3 = new Input();
+//        input3.getInt(1,100, "Enter a number between");
+
+//        Input input4 = new Input();
+//        System.out.println(input4.getInt());
+
+//        Input input5 = new Input();
+//        input5.getDouble(1.00,100.00);
+
+//        Input input5 = new Input();
+//        input5.getDouble(1.00,100.00, "Enter a number between");
+    }
+}
